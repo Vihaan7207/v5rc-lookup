6dc8afc65ab0fea948be62cd908170b600ce5eef
Vihaan7207 <vmikki7207@gmail.com>
2025-12-12 15:12:49 -0500

    github pages test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined, // use '404.html' for SPA fallback routing
+            precompress: false,
+            strict: true
+        }),
+        // Set the base path to your GitHub repository name
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/v5rc-lookup' : ''
+        }
+    }
 };
 
 export default config;
